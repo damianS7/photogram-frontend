88794156cd58e90e8e03089d91672edb807002fc
Damian <josepwnz@gmail.com>
2025-08-06 00:14:18 +0200

    Added spinner. added initialized flag to main div.

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from "vue";
 import { useAuthStore } from "@/stores/auth";
+import { useCustomerStore } from "@/stores/customer";
+import { useSettingStore } from "@/stores/setting";
 import { useRouter } from "vue-router";
 import Header from "@/components/Header.vue";
-import Sidebar from "@/components/Sidebar.vue";
 import { useSpinnerStore } from "@/stores/spinner";
 const screenSpinner = useSpinnerStore();
 const authStore = useAuthStore();
+const customerStore = useCustomerStore();
+const settingStore = useSettingStore();
 const router = useRouter();
 const tokenValidationInterval = 30 * 1000; // 30s
 let interval: NodeJS.Timeout;
@@ -31,34 +34,25 @@ async function wait(ms: number) {
 }
 
 onMounted(async () => {
+  screenSpinner.show();
   interval = setInterval(async () => {
     await checkIfTokenIsValid();
   }, tokenValidationInterval);
 
-  //   await customerStore.initialize();
-  //   await settingStore.initialize();
+  await customerStore.initialize();
+  await settingStore.initialize();
   initialized.value = true;
   screenSpinner.hide();
 });
-
 onUnmounted(() => {
   clearInterval(interval);
 });
 </script>
 <template>
-  <main class="flex flex-col h-screen overflow-hidden">
+  <main v-if="initialized" class="flex flex-col">
     <Header />
-    <div class="flex h-full p-6 overflow-hidden w-full sm:max-w-sm mx-auto">
-      <div
-        class="main-container flex w-full rounded shadow border-2 border-gray-300 overflow-hidden"
-      >
-        <div>
-          <Sidebar />
-        </div>
-        <div class="w-full h-full">
-          <router-view />
-        </div>
-      </div>
+    <div class="p-6">
+      <router-view />
     </div>
   </main>
 </template>
