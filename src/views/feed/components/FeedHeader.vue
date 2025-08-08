f764264b8924d7f265d18cb43f9ea39ce9b658e9
Damian <josepwnz@gmail.com>
2025-08-08 23:51:11 +0200

    functional feedheader

--- src/views/feed/components/FeedHeader.vue
+++ src/views/feed/components/FeedHeader.vue
@@ -1,42 +1,54 @@
 <script setup lang="ts">
 import { useCustomerStore } from "@/stores/customer";
-import ProfilePhoto from "@/components/ProfilePhoto.vue";
-const customerStore = useCustomerStore();
+import type { Feed } from "@/types/Feed";
+const props = defineProps<{
+  feed: Feed;
+}>();
+
+function isLoggedUserFeed() {
+  const loggedUserCustomerId = useCustomerStore().customer.id;
+
+  return loggedUserCustomerId === props.feed.customerId ? true : false;
+}
 </script>
 <template>
   <div class="flex sm:max-w-xl p-2 gap-4 mx-auto">
     <!-- Columna 1: Foto -->
     <div class="">
-      <ProfilePhoto class="rounded-full max-w-32 bg-gray-300" />
+      <img
+        :src="feed.profileImageFilename"
+        alt="Profile photo"
+        class="rounded-full max-w-32 bg-gray-300"
+      />
     </div>
 
     <!-- Columna 2: 3 filas -->
     <div class="flex flex-col justify-center gap-2">
       <div class="flex items-center gap-2">
-        <b class="uppercase">{{ customerStore.customer.profile?.username }}</b>
-        <button class="btn btn-sm btn-primary">Follow</button>
-        <button class="btn btn-sm btn-primary">Send message</button>
+        <b class="uppercase">@{{ feed.username }}</b>
+        <div class="flex items-center gap-2" v-if="!isLoggedUserFeed()">
+          <button class="btn btn-sm btn-primary">Follow</button>
+          <button class="btn btn-sm btn-primary">Send message</button>
+        </div>
       </div>
       <div class="flex items-center gap-4">
         <span class="text-gray-500"
           >Posts:
 
-          <b>0</b>
+          <b>{{ feed.totalPosts }}</b>
         </span>
         <span class="text-gray-500"
           >Followers:
-          <b>0</b>
+          <b>{{ feed.followers }}</b>
         </span>
         <span class="text-gray-500"
           >Follows:
-          <b>0</b>
+          <b>{{ feed.followed }}</b>
         </span>
       </div>
       <div class="flex items-center gap-4 text-xs text-gray-400">
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ullam saepe culpa nihil
-          eos fugiat, placeat fugit velit reprehenderit, ea minima non consequuntur quia quidem
-          pariatur odio facere, fuga quo?
+          {{ feed.aboutMe }}
         </p>
       </div>
     </div>
