55a344f56381adbf3bb5b58eeebb20bddf092284
Damian <josepwnz@gmail.com>
2025-08-15 20:22:42 +0200

    implementation of composable to detect scroll when hits bottom

--- src/components/post/comment/CommentList.vue
+++ src/components/post/comment/CommentList.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { dateUtils } from "@/utils/date";
 import { useCommentStore } from "@/stores/comment";
-import { computed, onMounted, onUnmounted, ref } from "vue";
+import { computed, onMounted, ref, useTemplateRef } from "vue";
+import { useScrollBottonDetect } from "@/composables/useScrollBottomDetect";
 const { toDatetime } = dateUtils();
 
 // props
@@ -13,76 +14,58 @@ const props = defineProps<{
 const commentStore = useCommentStore();
 
 // data
-const commentListRef = ref<HTMLDivElement | null>(null);
 const page = ref(0);
 const comments = computed(() => {
   return commentStore.comments;
 });
 
+const commentListRef = useTemplateRef("commentListRef");
+useScrollBottonDetect(commentListRef, doOnBottom);
+
 // functions
-async function detectBottom() {
-  const { scrollTop, scrollHeight, clientHeight } = commentListRef.value as HTMLDivElement;
-
-  // detect when hits the bottom
-  if (scrollTop + clientHeight >= scrollHeight) {
-    // if page is the last one, do nothing
-    if (
-      commentStore.pagination?.totalPages &&
-      page.value >= commentStore.pagination?.totalPages - 1
-    ) {
-      return;
-    }
-
-    // next page
-    page.value += 1;
-
-    // fetch comments for the next page
-    await commentStore.fetchComments(props.postId, page.value);
+async function doOnBottom() {
+  if (
+    commentStore.pagination?.totalPages &&
+    page.value >= commentStore.pagination?.totalPages - 1
+  ) {
+    return;
   }
+
+  // next page
+  page.value += 1;
+
+  // fetch comments for the next page
+  await commentStore.fetchComments(props.postId, page.value);
 }
 
 // lifecycle hooks
 onMounted(async () => {
-  // disable body scroll
-  document.body.style.overflow = "hidden";
   await commentStore.fetchComments(props.postId);
-  if (commentListRef.value) {
-    commentListRef.value.addEventListener("scroll", detectBottom);
-  }
-});
-
-onUnmounted(() => {
-  document.body.style.overflow = "";
-  if (commentListRef.value) {
-    commentListRef.value.removeEventListener("scroll", detectBottom);
-  }
 });
 </script>
 <template>
-  <div
-    ref="commentListRef"
-    v-if="comments.length > 0"
-    class="flex flex-col overflow-y-auto p-4 h-full gap-2"
-  >
+  <div ref="commentListRef" class="flex flex-col overflow-y-auto p-4 h-full gap-2">
     <TransitionGroup name="fade" tag="div" class="space-y-2">
-      <div
-        v-for="comment in comments"
-        :key="comment.id"
-        class="flex flex-col border-2 border-dashed py-1 px-2 rounded bg-gray-100"
-      >
-        <span class="py-2 text-sm">
-          {{ comment.content }}
-        </span>
-        <span class="flex justify-between items-center text-xs text-gray-500">
-          <strong>{{ comment.username }}</strong>
-          <span class="italic">
-            {{ toDatetime(comment.createdAt) }}
+      <slot v-if="comments.length > 0">
+        <div
+          v-for="comment in comments"
+          :key="comment.id"
+          class="flex flex-col border-2 border-dashed py-1 px-2 rounded bg-gray-100"
+        >
+          <span class="py-2 text-sm">
+            {{ comment.content }}
           </span>
-        </span>
-      </div>
+          <span class="flex justify-between items-center text-xs text-gray-500">
+            <strong>{{ comment.username }}</strong>
+            <span class="italic">
+              {{ toDatetime(comment.createdAt) }}
+            </span>
+          </span>
+        </div>
+      </slot>
+      <div v-else class="text-center text-sm">No comments found.</div>
     </TransitionGroup>
   </div>
-  <div v-else class="p-4 text-center text-sm">No comments found.</div>
 </template>
 <style scoped>
 .fade-enter-from {
