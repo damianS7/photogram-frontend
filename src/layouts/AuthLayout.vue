a7de7587f1412c0d4725c2573dcdfff612297643
Damian <josepwnz@gmail.com>
2025-08-26 21:05:37 +0200

    remove logo from view and added to layout

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -4,6 +4,9 @@ const route = useRoute();
 </script>
 <template>
   <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100 px-2 py-8">
+    <div class="w-full text-center mb-8">
+      <span class="logo" to="/"> Photogram </span>
+    </div>
     <div class="w-full max-w-md">
       <router-view></router-view>
     </div>
@@ -22,6 +25,26 @@ const route = useRoute();
       >
         ¿Dont you have an account? Sign up
       </RouterLink>
+      <RouterLink
+        v-if="!route.path.includes('/accounts/reset-password')"
+        to="/accounts/reset-password"
+        class="block hover:underline text-blue-600"
+      >
+        ¿Forgot your password?
+      </RouterLink>
+      <RouterLink
+        v-if="!route.path.includes('/accounts/activate')"
+        to="/accounts/activate"
+        class="block hover:underline text-blue-600"
+      >
+        Activate account
+      </RouterLink>
     </div>
   </div>
 </template>
+<style scoped>
+.logo {
+  font-family: "Grand Hotel", cursive;
+  font-size: 3.6rem;
+}
+</style>
